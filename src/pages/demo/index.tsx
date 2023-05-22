501c54a1c66d95fc5332a291dc184af71e6cbd9c
Jack-Desktop <jackpinkerton@live.com.au>
2023-05-22 21:13:33 +1000

    constructing demo page and properties component

--- src/pages/demo/index.tsx
+++ src/pages/demo/index.tsx
@@ -1,9 +1,15 @@
 import { NextPage } from "next";
 
+const properties = {apartment: null, streetnumber: '47', street: 'Donnelly Street', suburb: 'Balmain', state: 'NSW', country: 'Australia', lastjob: 'Repair Leaking Tap'} 
+
 const DemoPage: NextPage = () => {
 
     return(
-        <>Demo Page</>
+        <>
+            <h1>Demo Page - Trade Dashboard</h1>
+            <h1>Properties</h1>
+            <h1>Recents Jobs</h1>
+        </>
     )
 }
 
