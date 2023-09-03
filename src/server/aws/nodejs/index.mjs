7bcb093f1cb09bc897c695e640bdcc5d4a695b6e
Jack Pinkerton <jack@fidelitysystems.com.au>
2023-09-03 12:29:46 +1000

    lambda handler file updated

---
+++ src/server/aws/nodejs/index.mjs
@@ -0,0 +1,149 @@
+// dependencies
+import { S3, GetObjectCommand } from "@aws-sdk/client-s3";
+import sharp from "sharp";
+//const util = require('util');
+//const sharp = require('sharp');
+
+export const handler = async (event) => {
+
+    // Read options from the event parameter.
+    //console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
+    // Object key may have spaces or unicode non-ASCII characters.
+    const [path, size] = getQueryStringParameters(event);
+    const [user, category, file] = path.split("/");
+
+    // Infer the image type from the file suffix.
+    try {
+        checkImageType(file);
+        console.log("Image type Accepted");
+    } catch (error) {
+        console.log("Problem checking image type");
+        console.log(error);
+    }
+    
+
+    // Download the image from the S3 source bucket.
+    const s3 = new S3({
+        region: process.env.REGION,
+        credentials: {
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+        },
+    }); 
+
+    const orignalImage = await getImageFroms3(path, s3);
+    let resizedImage
+    if (size === "full") {
+        resizedImage = await convertImage(orignalImage);
+    } else {
+        resizedImage = await convertAndResizeImage(orignalImage, size);
+    }
+    
+
+    console.log(resizedImage);
+    let response = {
+        statusCode: 200,
+        body: JSON.stringify("Got image")
+    };
+    return response;
+};
+
+const getQueryStringParameters = (event) => {
+    const path = decodeURIComponent(event.queryStringParameters.path);
+    const size = event.queryStringParameters.size;
+    return [path, size]
+}
+
+const checkImageType = (file) => {
+    const typeMatch = file.match(/\.([^.]*)$/);
+    if (!typeMatch) {
+        console.log("Could not determine the image type.");
+        throw "Could Not Determine Image Type";
+    }
+
+    // Check that the image type is supported
+    const imageType = typeMatch[1].toLowerCase();
+    if (imageType != "jpg" && imageType != "png" && imageType != "jpeg" && imageType != "tiff") {
+        console.log(`Unsupported image type: ${imageType}`);
+        throw "Unsupported Iamge type, jpg png jpeg and tiff only supported currently";
+    }
+};
+
+const getImageFroms3 = async (path, s3) => {
+    // get reference to S3 client
+    
+    try {
+        const params = {
+            Bucket: "property-maintenance-app-photos",
+            Key:  path
+        };
+        console.log("params ", params);
+        var origimage = await s3.getObject(params).promise();
+        const command = new GetObjectCommand(params);
+        const response = await s3.send(command);
+        console.log("Got original image");
+        return response;
+  
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+}
+
+const convertAndResizeImage = async (originalImage, size) => {
+    // set thumbnail width. Resize will set the height automatically to maintain aspect ratio.
+    let width;
+    if (size === "sm") width = 300;
+    if (size === "md") width = 450;
+
+    // Use the sharp module to resize the image and save in a buffer.
+    try {
+        var buffer = await sharp(originalImage.Body)
+            .toFormat('jpg')
+            .resize(width, width)
+            .withMetadata()
+            .toBuffer();
+        return buffer;
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+}
+
+const convertImage = async (originalImage) => {
+    try {
+        var buffer = await sharp(originalImage.Body)
+            .toFormat('jpg')
+            .withMetadata()
+            .toBuffer();
+        return buffer;
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+}
+/*
+const uploadImageToNewPath = async (path, buffer, s3) => {
+
+    // Upload the thumbnail image to the destination bucket
+    try {
+        const destparams = {
+            Bucket: "property-maintenance-app-photos",
+            Key: path,
+            Body: buffer,
+            ContentType: "image"
+        };
+
+        console.log("destparams ", destparams);
+
+        const putResult = await s3.putObject(destparams).promise();
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+}
+
+*/
