b76bfe23458c08b3d7e97c3c48ff622a493379d3
Jack-Desktop <jackpinkerton@live.com.au>
2023-09-11 20:01:43 +1000

    Photo component updated for prisma data model

--- src/components/Photos.tsx
+++ src/components/Photos.tsx
@@ -1,11 +1,16 @@
 /*
-    Will need to get signed url from server
-    then use url for loading image   */
-import Image from "next/image"
+    Will need to add a full size image popover, which lets   */
+import Image from "next/image";
+import { RouterOutputs, api } from '~/utils/api'
+import Popover from "./Popover";
+import { useState } from "react";
 
+type Photo = RouterOutputs["photo"]["getUnassignedPhotosForJob"][number];
+
+type Photos = RouterOutputs["photo"]["getUnassignedPhotosForJob"];
 
 type Props = {
-    photos: IPhoto[]
+    photos: Photos
 }
 
 const Photos: React.FC<Props> = ({ photos }) => {
@@ -24,18 +29,29 @@ const Photos: React.FC<Props> = ({ photos }) => {
 export default Photos;
 
 type PhotoProps = {
-    photo: IPhoto
+    photo: Photo
 }
 
+
 const Photo: React.FC<PhotoProps> = ({ photo }) => {
-
+    const [ fullSizePhotoOpen, setFullSizePhotoOpen] = useState(false);
+    const {data: url} = api.photo.getPhoto.useQuery({name: photo.filename, type: "sm"})
+    console.log("get photo url ", url);
+    if (typeof url !== 'string') return <>Loading</>
     return(
-        <Image
-                        src={photo.url}
-                        alt={photo.filename}
-                        width={130}
-                        height={130}
-                        />
+        <>
+            <Popover popoveropen={fullSizePhotoOpen} setPopoverOpen={setFullSizePhotoOpen}>
+                <FullSizePhoto photo={photo} />
+            </Popover>
+                <Image src={url} width={220} height={220} alt="image" onClick={() => setFullSizePhotoOpen(true)}/>
+        </>
+    )
+}
 
+const FullSizePhoto: React.FC<PhotoProps> = ({ photo }) => {
+    const {data: url} = api.photo.getPhoto.useQuery({name: photo.filename, type: "full"})
+    if (typeof url !== 'string') return <>Loading</>
+    return(
+         <Image src={url} width={300} height={300} alt="image" />
     )
 }
