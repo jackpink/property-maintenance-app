5370519b7f52e24cafb169c2d35f1384bdce40b2
Jack-Desktop <jackpinkerton@live.com.au>
2023-10-09 17:07:29 +1100

    address search bar added, address validation not working yet

--- src/pages/homeowner/index.tsx
+++ src/pages/homeowner/index.tsx
@@ -1,7 +1,7 @@
 import { useAuth } from "@clerk/nextjs";
 import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Button from "~/components/Button";
 import Popover from "~/components/Popover";
 import Properties from "~/components/Properties";
@@ -86,20 +86,116 @@ const CreateProperty: React.FC<CreatePropertyProps> = ({ userId }) => {
     </div>
   );
 };
+const getValidAddressViaGoogle = async (addressSearchString: string) => {
+  const client = axios.create();
+  const googleAddressValidationEndpoint =
+    "https://addressvalidation.googleapis.com/v1:validateAddress?key=" +
+    env.GOOGLE_MAPS_API_KEY;
+  const requestBody = {
+    address: {
+      regionCode: "AU",
+      addressLines: [addressSearchString],
+    },
+  };
+  const response = await client.post(
+    googleAddressValidationEndpoint,
+    requestBody
+  );
+
+  const validAddressDecomposed = {
+    addressString: response.result.address.formattedAddress,
+  };
+  return validAddressDecomposed;
+};
 
 type CreatePropertyFormProps = {
   userId: string;
 };
 
+type ValidAddress = {
+  addressString: string;
+};
+
 const CreatePropertyForm: React.FC<CreatePropertyFormProps> = ({ userId }) => {
+  const [addressSearchTerm, setAddressSearchTerm] = useState("");
+  const [validAddress, setValidAddress] = useState<ValidAddress>({
+    addressString: "",
+  });
+
+  const getValidAddress = useCallback(() => {
+    getValidAddressViaGoogle(addressSearchTerm).then((response) => {
+      setValidAddress((prev) => ({
+        ...prev,
+        addressString: response.addressString,
+      }));
+      console.log("valid address response", response);
+    });
+  }, [addressSearchTerm]);
+
   return (
     <div className="grid justify-items-center">
       <h1 className="block text-2xl font-medium text-gray-700">
         Create Property
       </h1>
-      <p>address search bar</p>
-      <p>Selected Address and info</p>
+      <AddressSearch
+        setAddressSearchTerm={setAddressSearchTerm}
+        onClickSearch={getValidAddress}
+      />
+      <p>{validAddress?.addressString}</p>
       <Button>Create Property</Button>
+    </div>
+  );
+};
+
+import { type Dispatch, type SetStateAction } from "react";
+import axios from "axios";
+import { env } from "~/env.mjs";
+
+type Props = {
+  setAddressSearchTerm: Dispatch<SetStateAction<string>>;
+  onClickSearch: () => void;
+};
+
+const AddressSearch: React.FC<Props> = ({
+  setAddressSearchTerm,
+  onClickSearch,
+}) => {
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setAddressSearchTerm(event.currentTarget.value);
+  };
+
+  return (
+    <div className="w-full md:w-3/4 xl:w-1/2">
+      <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+        <input
+          type="search"
+          className="focus:border-primary dark:focus:border-primary relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-teal-700 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(13,148,136)] focus:outline-none dark:border-teal-600 dark:text-teal-200 dark:placeholder:text-neutral-200"
+          placeholder="Search by suburb or postcode"
+          onChange={handleChange}
+        />
+
+        <button
+          className="relative z-[2] flex items-center rounded-r border border-solid border-teal-700 bg-teal-500 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-teal-700 hover:shadow-lg focus:bg-teal-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-teal-800 active:shadow-lg"
+          type="button"
+          id="button-addon1"
+          data-te-ripple-init
+          data-te-ripple-color="light"
+          onClick={onClickSearch}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="h-5 w-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
