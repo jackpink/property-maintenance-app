9992c930e8d22653cb1327f165907450cace8567
Jack-Desktop <jackpinkerton@live.com.au>
2023-10-15 20:56:58 +1100

    trade info interface added

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,38 +1,12 @@
 export {}
 
 declare global {
-  interface IRoom {
-    label: string
-    order: number
+  interface ITradeInfo {
+    name: string | null
+    email: string | null
+    phone: string | null
   }
-  interface ILevel {
-    label: string
-    order: number
-    rooms: IRoom[]
-  }
-  interface IProperty {
-    apartment: string | null
-    streetNumber: string
-    street: string
-    suburb: string
-    postcode: string
-    state: string
-    country: string
-  }
-  interface IJob {
-    id: string
-    title: string
-    photos: IPhoto[]
-    date: Date
-    Property: IProperty
-  }
-  interface IDocument {
-    name: string
-  }
-  interface IPhoto {
-    filename: string
-    url: string
-  }
+
   interface IAddress {
     apartment: string | null
     streetNumber: string
