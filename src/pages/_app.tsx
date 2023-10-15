83698b22d6231cb79d1c736e16f548864844cbc3
Jack-Desktop <jackpinkerton@live.com.au>
2023-10-15 21:00:07 +1100

    sonner package added for Toast UI options

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { type AppType } from "next/app";
-import { ClerkProvider } from '@clerk/nextjs'
-
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 import { api } from "~/utils/api";
-
+import "react-day-picker/dist/style.css";
 import "~/styles/globals.css";
 import PageLayout from "~/components/layout";
 
@@ -10,11 +10,11 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <PageLayout>
+        <Toaster position="top-center" richColors />
         <Component {...pageProps} />
       </PageLayout>
     </ClerkProvider>
-  )
-  
+  );
 };
 
 export default api.withTRPC(MyApp);
