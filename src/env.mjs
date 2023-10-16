a7d5853bea2c0389065637f419861aab9a003afc
Jack-Desktop <jackpinkerton@live.com.au>
2023-10-16 19:12:38 +1100

    env file updated to include document bucket info

--- src/env.mjs
+++ src/env.mjs
@@ -13,7 +13,8 @@ export const env = createEnv({
     AWS_SECRET_ACCESS_KEY: z.string(),
     PHOTO_BUCKET_NAME: z.string(),
     REGION: z.string(),
-    GOOGLE_MAPS_API_KEY: z.string()
+    GOOGLE_MAPS_API_KEY: z.string(),
+    DOCUMENT_BUCKET_NAME: z.string()
   },
 
   /**
@@ -36,7 +37,8 @@ export const env = createEnv({
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     PHOTO_BUCKET_NAME: process.env.PHOTO_BUCKET_NAME,
     REGION: process.env.REGION,
-    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY
+    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+    DOCUMENT_BUCKET_NAME: process.env.DOCUMENT_BUCKET_NAME
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
 });
