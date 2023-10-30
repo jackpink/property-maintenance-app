fd99b350e469fabca58cfcdae53056a8d676896f
Jack-Desktop <jackpinkerton@live.com.au>
2023-10-30 19:17:57 +1100

    concat address function added to utlis

--- src/utils/utits.ts
+++ src/utils/utits.ts
@@ -5,3 +5,32 @@ export function FormattedDate(date?: Date) {
     date && (formattedDate = format(date, "PPP"));
     return formattedDate;
   }
+
+interface PropertyWithoutJobs {
+  streetNumber: string;
+  street: string;
+  suburb: string;
+  state: string;
+  country: string;
+  apartment: string | null;
+};
+//RouterOutputs["job"]["getRecentJobsForTradeUser"][number]["Property"];
+
+export const concatAddress = (property: PropertyWithoutJobs) => {
+  let address =
+    property.streetNumber +
+    " " +
+    property.street +
+    ", " +
+    property.suburb +
+    ", " +
+    property.state +
+    ", " +
+    property.country;
+  if (!!property.apartment) {
+    // add apartment number in front /
+
+    address = property.apartment + " / " + address;
+  }
+  return address;
+};
