3d883e8fb050552ec3d02dfe0a3069a171341316
Jack-Desktop <jackpinkerton@live.com.au>
2023-11-13 17:23:21 +1100

    fixed day picker highlight issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 import { api } from "~/utils/api";
-import "react-day-picker/dist/style.css";
 import "~/styles/globals.css";
+import "react-day-picker/dist/style.css";
 import PageLayout from "~/components/Atoms/layout";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
