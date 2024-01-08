d86428a39568226feaa9b18fda0ef1d4e825dfa2
Jack-Desktop <jackpinkerton@live.com.au>
2024-01-08 13:12:46 +1100

    redirect page created, not properly woring yet

---
+++ src/pages/redirect/index.tsx
@@ -0,0 +1,15 @@
+/*
+check if user is a homeowner, if true redirect to /properties
+check if user is linked to an organization, if true redirect to /contractor
+else redirect to error page
+*/
+
+const RedirectPage = () => {
+  return (
+    <div>
+      <h1>Redirecting</h1>
+    </div>
+  );
+};
+
+export default RedirectPage;
