78db90b377de05443b12ca14461f26b88ab0a4df
Jack-Desktop <jackpinkerton@live.com.au>
2024-01-08 17:13:26 +1100

    create contractor pages still being updated

--- src/pages/redirect/index.tsx
+++ src/pages/redirect/index.tsx
@@ -4,11 +4,23 @@ check if user is linked to an organization, if true redirect to /contractor
 else redirect to error page
 */
 
+import Link from "next/link";
+import { GenericUserPageRedirect } from "~/components/Atoms/UserRedirects";
+
 const RedirectPage = () => {
   return (
-    <div>
-      <h1>Redirecting</h1>
-    </div>
+    <GenericUserPageRedirect>
+      <div>
+        <h1>Error</h1>
+        <p>
+          Your account is enither showing as a homeowner account or is connected
+          to a Contractor account. If you'd like to create a new contractor
+          account for{" "}
+          <Link href="/create-account/contractor/organisation">click here</Link>
+          . Otherwise please contact support.
+        </p>
+      </div>
+    </GenericUserPageRedirect>
   );
 };
 
