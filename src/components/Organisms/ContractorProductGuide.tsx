ddfdee53daf42f412e264f07f2b2ed4b232a5526
Jack-Desktop <jackpinkerton@live.com.au>
2024-01-19 11:55:32 +1100

    created guides display

--- src/components/Organisms/ContractorProductGuide.tsx
+++ src/components/Organisms/ContractorProductGuide.tsx
@@ -1,10 +1,14 @@
-import { api } from "~/utils/api";
+import { RouterOutputs, api } from "~/utils/api";
 import {
+  DocumentIcon,
+  GhostButton,
   LargeButton,
   LargeButtonContent,
   LargeButtonTitle,
 } from "../Atoms/Button";
 import router, { useRouter } from "next/router";
+import { EditIconSmall, GreenTickIcon, ViewIcon } from "../Atoms/Icons";
+import Link from "next/link";
 
 export const AddGuide = ({
   productId,
@@ -36,5 +40,46 @@ export const AddGuide = ({
         Create a new Guide to be added to this list
       </LargeButtonContent>
     </LargeButton>
+  );
+};
+
+type Guides = RouterOutputs["product"]["getProduct"]["guides"];
+
+export const ProductGuides = ({ guides }: { guides: Guides }) => {
+  return (
+    <>
+      {guides.map((guide) => (
+        <Guide key={guide.id} guide={guide} />
+      ))}
+    </>
+  );
+};
+
+const Guide = ({ guide }: { guide: Guides[0] }) => {
+  const path = useRouter().asPath;
+  return (
+    <div className="flex w-full flex-nowrap	items-center gap-1 sm:gap-4">
+      <GreenTickIcon width={28} height={28} />
+      <DocumentIcon width="40" height="40" />
+      <div className="grow text-left">
+        <p className="text-lg font-medium md:pl-10">{guide.label}</p>
+      </div>
+      <div className="flex grow-0 gap-4 ">
+        <GhostButton onClick={() => console.log()}>
+          <div className="flex">
+            <ViewIcon />
+            <p className="hidden pl-2 lg:block">VIEW</p>
+          </div>
+        </GhostButton>
+        <Link href={`${path}/${guide.id}`}>
+          <GhostButton>
+            <div className="flex">
+              <EditIconSmall />
+              <p className="hidden pl-2 lg:block">Edit</p>
+            </div>
+          </GhostButton>
+        </Link>
+      </div>
+    </div>
   );
 };
